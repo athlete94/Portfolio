a4c6e33b5ee933d68de349dfb3dc1d3d6045a274
Azamat <azamt14@gmail.com>
2022-04-03 23:33:33 +0300

    first commit

---
+++ src/header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Nav.module.css'
+
+export const Nav = () => {
+    return (
+        <div className={s.nav}>
+
+        </div>
+    );
+};
---
+++ src/skills/Skill/Skill.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './Skills.module.css'
+
+type SkillsPropsType = {
+    title: string
+    description: string
+}
+
+export const Skill = ({title, description}: SkillsPropsType) => {
+    return (
+        <div className={s.skillsItem}>
+            <div className={s.icon}>
+            </div>
+            <h3>{title}</h3>
+            <span className={s.description}>{description}</span>
+        </div>
+    );
+};
