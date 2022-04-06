5185690f6734750820e255f34fe8c0524e3850a6
Azamat <azamt14@gmail.com>
2022-04-06 21:14:48 +0300

    refactor css

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -6,7 +6,7 @@ import styleContainer from '../common/styles/Container.module.css'
 export const Header = () => {
     return (
         <div className={s.header}>
-            <div className={`${styleContainer.container} ${s.headerContainer}`} >
+            <div className={`${s.headerContainer} ${styleContainer.container}`}>
                 <Nav />
             </div>
         </div>
